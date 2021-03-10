dada5ae199ef9feb7c9f2dc59fb1bd994b5b405c
Eliott <eliott_nu@mail.ru>
2021-03-10 10:41:51 +0900

    Task #1

---
+++ HomeWork #1/Assets/__Scripts/Movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public CharacterController controller; // Игрок
+    public float speed = 5f; // Cкорость движения
+    public float turnSmoothTime = 0.1f; // Скорость поворота
+
+    [Header("Set is Dynamically")]
+    private float turnSmoothVelocity;
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            // Поворот модели
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+
+            // Движение по сторонам x, y
+            controller.Move(direction * speed * Time.deltaTime);
+        }
+    }
+}
